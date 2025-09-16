7a4f7236cc68a96fc75d5d9e06056b0e833036bc
Michael <mw78396@gmail.com>
2025-09-16 10:16:54 -0400

    Changed file name and started lay out

---
+++ MathGameL1.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+let GameMode = Number(prompt(`Please select an option - \n\tPress 1 to play Max Score \n\tPress 2 to play three-out \n\tPress 3 to exit`))
+if(option === 1){
+    let MaxScore = Number(prompt(``))
+}
+if(option === 2){
+
+}
+if(option === 3){
+
+}

8bfbf0ab7d9cca8ddfe4d78077b0d1db08401678
Michael <mw78396@gmail.com>
2025-09-16 11:31:37 -0400

    trying to debug

--- MathGameL1.js
+++ MathGameL1.js
@@ -1,32 +1,42 @@
 const prompt = require('prompt-sync')();
-
-let GameMode = Number(prompt(`Please select an option - \n\tPress 1 to play Max Score \n\tPress 2 to play three-out \n\tPress 3 to exit`))
-if(option === 1){
-    function mathQuestions(){
-        const eQ = [`+`,`-`,`*`,`/`,`%`]
-        const Num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        for (let i=0; i<10; i++){
-            const num1 = Num[i];
-            const num2 = Num[(i+3) % Num.length]+1;
-            const eQs = eQ[i%eQ.length];
-            let Q;
-            if(eQ === `/`){
-                const div = num1 * num2;
-                Q=`${div}/${num2}`;
-            }else if(eQ === `%`){
-                const div = num1 + num2;
-                Q=`${div}%${num2}`
-            }else{
-                Q=`${num1}${eQ}${num2}`
-            }
-            console.log(`Question: ${i+1}:${Q}`)
-        }
+while(true){
+    let option = Number(prompt(`Please select an option - \n\tPress 1 to play Max Score \n\tPress 2 to play three-out \n\tPress 3 to exit \n\tOption: `))
+    console.log(`typeos option:`, typeof option, "| Value:", option);
+    console.log(`You selected: `,option)
+    if(option === 1){
+        mathQuestions();
     }
-    mathQuestions();
-}
-if(option === 2){
+    else if(option === 2){
 
+    }
+    else if(option === 3){
+        break;
+    }
+    else{
+        console.log(`Invalid option, program end.`)
+        break;
+    }
 }
-if(option === 3){
 
+function mathQuestions(){
+    console.log(`mathQuestions started`)
+    const eQ = [`+`,`-`,`*`,`/`,`%`]
+    const Num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for (let i=0; i<20; i++){
+        const num1 = Num[i % Num.length];
+        const num2 = Num[(i + 3) % Num.length] + 1;
+        const eQs = eQ[i % eQ.length];
+        let Q;
+        if(eQs === `/`){
+            const div = num1 * num2;
+            Q=`${div} / ${num2}`;
+        }else if(eQs === `%`){
+            const div = num1 + num2;
+            Q=`${div} % ${num2}`
+        }else{
+            Q=`${num1} ${eQs} ${num2}`
+        }
+        console.log(`Question: ${i+1}:${Q}`)
+    }
+    console.log(`mathQuestions end`)
 }
